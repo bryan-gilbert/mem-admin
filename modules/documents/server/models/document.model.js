254ba85f704c41eb854603788a67343b486836df
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-07-25 17:05:13 -0700

    Fix inspection type calculation. Accept categories with sub-categories. Reject other categories and happen to include text: inspection report

--- modules/documents/server/models/document.model.js
+++ modules/documents/server/models/document.model.js
@@ -83,7 +83,7 @@
 //
 // =========================================================================
 var genSchema = require ('../../../core/server/controllers/core.schema.controller');
-
+var _          = require ('lodash');
 genSchema ('TypesSchema', {
 	projectFolderType           : { type:String, default:'' },
 	projectFolderSubTypeObjects : []
@@ -181,14 +181,19 @@ module.exports = genSchema ('Document', {
 
 function inspectionType() {
 	var categories = this.documentCategories || [];
-	var result = null;
-	for(var i = 0; i < categories.length; i++) {
+	// see copy of this same code on client: doc-categories.js
+	var inspectionReportTypes = ['Inspection Report Response', 'Inspection Report Follow Up', 'Inspection Report'];
+	var inspectionType = null;
+	for(var i = 0; i < categories.length && !inspectionType; i++) {
 		var cat = categories[i];
-		if (cat.indexOf("Inspection ") >= 0) {
-			result = cat;
-			break;
+		for (var k=0; k < inspectionReportTypes.length && !inspectionType; k++) {
+			var type = inspectionReportTypes[k];
+			if (_.startsWith(cat,type)) {
+				inspectionType = type;
+			}
 		}
 	}
-	return result;
+	return inspectionType;
 }
+
 
